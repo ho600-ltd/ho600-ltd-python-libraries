7eed490b5f77641c22d9f329546658ce3c6fbb93
hoamon h97progamer <hoamon@ho600.com>
2021-11-03 16:40:06 +0800

    Finish integer_from_customize_hex function @for-integer_from_customize_hex]

--- ho600_ltd_libraries/utils/formats.py
+++ ho600_ltd_libraries/utils/formats.py
@@ -20,13 +20,19 @@ def integer_from_customize_hex(string, base='0123456789abcdef'):
         convert '11111' with binary-string(01) will be int(31)
         convert int(31) with digit-string(0-9) will be '31'
         convert int(31) with hex-string(0-9a-f) will be '1f'
-        convert int(31) with base-string(a-fg-v0-5) will be '5'
+        convert int(31) with base-string(a-fg-v0-5) will be 'bd'
+        convert int(27) with base-string(a-fg-v0-5) will be '5'
         convert int(63) with base-string(abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.,) will be ','
         convert int(16777215) with base-string(abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.,) will be ',,,,'
         convert int(16777216) with base-string(abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.,) will be 'baaaa'
     """
+    position_dict = {}
     base_len = len(base)
-    if number < base_len:
-        return base[number]
-    else:
-        return customize_hex_from_integer(number//base_len, base) + base[number%base_len]
+    for i, s in enumerate(base):
+        position_dict[s] = i
+    string_len = len(string)
+    number = 0
+    for i, s in enumerate(string):
+        _n = position_dict[s] * base_len ** (string_len - i - 1)
+        number += _n
+    return number
--- ho600_ltd_libraries/utils/tests.py
+++ ho600_ltd_libraries/utils/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ho600_ltd_libraries.utils.formats import customize_hex_from_integer
+from ho600_ltd_libraries.utils.formats import customize_hex_from_integer, integer_from_customize_hex
 
 class FormatsTest(unittest.TestCase):
     def test_customize_hex_from_integer(self):
@@ -17,6 +17,18 @@ class FormatsTest(unittest.TestCase):
         self.assertEqual('10', customize_hex_from_integer(32, base='0123456789abcdefghijklmnopqrstuv'))
         self.assertEqual('vv', customize_hex_from_integer(1023, base='0123456789abcdefghijklmnopqrstuv'))
         self.assertEqual('100', customize_hex_from_integer(1024, base='0123456789abcdefghijklmnopqrstuv'))
+
+
+    def test_integer_from_customize_hex(self):
+        self.assertEqual(31, integer_from_customize_hex('11111', base='01'))
+        self.assertEqual(31, integer_from_customize_hex('31', base='0123456789'))
+        self.assertEqual(31, integer_from_customize_hex('1f', base='0123456789abcdef'))
+        self.assertEqual(27, integer_from_customize_hex('5', base='abcdefghijklmnopqrstuv012345'))
+        self.assertEqual(31, integer_from_customize_hex('bd', base='abcdefghijklmnopqrstuv012345'))
+        self.assertEqual(63, integer_from_customize_hex(',', base='abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.,'))
+        self.assertEqual(16777215, integer_from_customize_hex(',,,,', base='abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.,'))
+        self.assertEqual(16777216, integer_from_customize_hex('baaaa', base='abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.,'))
+
 
 if __name__ == '__main__':
     unittest.main()
