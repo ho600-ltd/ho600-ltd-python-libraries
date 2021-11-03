31ff1dbd3aa9e08a7f897571cf99c28f1f15a850
hoamon h97progamer <hoamon@ho600.com>
2021-11-03 16:40:42 +0800

    0.0.3 @master]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ho600-ltd-python-libraries",
-    version="0.0.2",
+    version="0.0.3",
     author="ho600 Ltd.",
     author_email="libraries@ho600.com",
     description="Utils and Libraries collection from ho600 Ltd.",
